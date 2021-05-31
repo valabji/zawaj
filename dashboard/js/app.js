d4a629d1464bff6f6fbb55ff9248820e7d61fa9a
mohamedmoa <30494857+mohamedmoa@users.noreply.github.com>
2021-05-31 02:11:49 +0200

    Bundles

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -235,6 +235,26 @@ angular.module("myApp",['ngAnimate', 'ngSanitize', 'ui.bootstrap','ui.router','a
         }
     })
 
+    .run(function($rootScope) {
+        $rootScope.typeOf = function(value) {
+            return typeof value;
+        };
+    })
+
+
+    .directive('stringToNumber', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(value) {
+                return '' + value;
+            });
+            ngModel.$formatters.push(function(value) {
+                return parseFloat(value);
+            });
+            }
+        };
+    })
 
     .factory('$localStorage', ['$window', function($window) {
         return {
