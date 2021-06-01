9a8b55569872f8cdbeaa3a47035fce29ab2cb890
valabji <valabji@gmail.com>
2021-06-01 21:09:35 +0200

    Search

--- api/app/Controllers/Users.php
+++ api/app/Controllers/Users.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, DELETE');
+$db = db_connect();
+
 // require APPPATH . '/Libraries/REST_Controller.php';
 // use Restserver\Libraries\REST_Controller;
 use CodeIgniter\RESTful\ResourceController;
