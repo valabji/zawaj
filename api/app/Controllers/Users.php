aa91a34fda958b55643ed2a40725cceca2fb7bac
valabji <valabji@gmail.com>
2021-06-01 23:25:28 +0200

    Search

--- api/app/Controllers/Users.php
+++ api/app/Controllers/Users.php
@@ -195,7 +195,7 @@ class Users extends ResourceController
     public function search()
     {
         $db = db_connect();
-        $cn = $db->query("select * from user");
+        $cn = $db->query("select * from user")->result();
 
         $st = new UserModel();
         $where = "true";
